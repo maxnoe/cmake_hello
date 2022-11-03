ac707030a666911e2c939123ae0c7d9719294803
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2022-11-03 10:55:09 +0100

    Use header sets

--- cli/fibonacci.cpp
+++ cli/fibonacci.cpp
@@ -7,16 +7,16 @@ using fibonacci::fibonacci_numbers;
 
 int main(int argc, char* argv[]) {
 	if (argc == 1 || argc > 2) {
-		std::cerr << "Usage fiboncacci <n> | fiboncacci -h | fiboncacci -v" << std::endl;
+		std::cerr << "Usage fibonacci <n> | fibonacci -h | fibonacci -v" << std::endl;
 		return 1;
 	}
 	std::string arg(argv[1]);
 
     if (arg == "-v" || arg == "--version") {
-        std::cout << "fiboncacci version " << FIBONACCI_VERSION << std::endl;
+        std::cout << "fibonacci version " << FIBONACCI_VERSION << std::endl;
     } else if (arg == "-h" || arg == "--help") {
         std::cerr << "Print the first N fibonacci numbersr" << std::endl << std::endl;
-        std::cerr << "Usage fiboncacci <n>" << std::endl;
+        std::cerr << "Usage fibonacci <n>" << std::endl;
     } else {
         int n = std::stoi(argv[1]);
         for (auto fib: fibonacci_numbers(n)) {
