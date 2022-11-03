81a8c2bbc07ee11314cc0497728746338c16052b
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2022-11-03 18:28:35 +0100

    Better solution for RPATH

--- cmake/RPATHHandling.cmake
+++ cmake/RPATHHandling.cmake
@@ -1,8 +1,15 @@
 # RPATH handling. This makes setting LD_LIBRARY_PATH unecessary
 # as it will compile the path to the libraries into the libraries / executables
+include(GNUInstallDirs)
+file(RELATIVE_PATH libdir_relative
+    ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_INSTALL_BINDIR}
+    ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}
+)
 if(APPLE)
-    list(APPEND CMAKE_INSTALL_RPATH "@executable_path/../${CMAKE_INSTALL_LIBDIR}")
+    list(APPEND CMAKE_INSTALL_RPATH @loader_path)
+    list(APPEND CMAKE_INSTALL_RPATH @executable_path/${libdir_relative})
 else()
-    list(APPEND CMAKE_INSTALL_RPATH "$ORIGIN/../${CMAKE_INSTALL_LIBDIR}")
+    list(APPEND CMAKE_INSTALL_RPATH $ORIGIN)
+    list(APPEND CMAKE_INSTALL_RPATH $ORIGIN/${libdir_relative})
 endif()
 set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
